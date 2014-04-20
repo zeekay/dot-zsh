24eaf67031aadf1296680af0344484838ec0d91c
Zach Kelling <zk@monoid.io>
2014-04-19 22:48:30 -0500

    Add ellipsis.sh files.

--- ellipsis.sh
+++ ellipsis.sh
@@ -1,17 +1,46 @@
 #!/usr/bin/env bash
+#
+# ellipsis-zsh
+#   ellipsis.sh module that install a zeesh-based zsh configuration.
 
 mod.install() {
-    ellipsis.run_installer "https://raw.githubusercontent.com/zeekay/zeesh/master/scripts/install.sh"
+    # backup existing files
+    ellipsis.backup ~/.zsh
+    ellipsis.backup ~/.zshrc
+    ellipsis.backup ~/.zshenv
+
+    # clone zeesh
+    git.clone "https://github.com/zeekay/zeesh" ~/.zsh
+
+    # symlink files
+    ln -s $mod_path/common/zshenv ~/.zsh/env
+    case "$(ellipsis.platform)"
+        darwin)
+            ellipsis.link_files "$mod_path/platform/osx"
+            ;;
+        freebsd)
+            ellipsis.link_files "$mod_path/platform/freebsd"
+            ;;
+        linux)
+            ellipsis.link_files "$mod_path/platform/linux"
+            ;;
+        cygwin*)
+            ellipsis.link_files "$mod_path/platform/cygwin"
+            ;;
+    esac
 }
 
 mod.pull() {
+    git.pull $mod_path
     git.pull ~/.zsh
 }
 
 mod.push() {
+    git.pull $mod_path
     git.push ~/.zsh
 }
 
 mod.status() {
+    git.pull $mod_path
     git.status ~/.zsh
 }
